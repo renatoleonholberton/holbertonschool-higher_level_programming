51fa5f63109702d9cb321c3745ddc60f1f1e6388
renatoleonholberton <2829@holbertonschool.com>
2021-04-27 17:18:00 +0000

    Remove use of chr function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for i in range(0, len(str)):
         if str[i] >= 'a' and str[i] <= 'z':
             ascii = ord(str[i])
-            print(chr(ascii - 32), end="")
+            print("{:c}".format(ascii - 32), end="")
         else:
             print(str[i], end="")
     print()
