62b6f3bb4510f299162216bd66388c3de1cf392a
renatoleonholberton <2829@holbertonschool.com>
2021-05-04 18:59:29 +0000

    Fix pointer reference to rev_head intead of curr

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -16,7 +16,7 @@ int is_palindrome(listint_t **head)
 	for (curr = *head; curr != NULL; curr = curr->next)
 		add_nodeint(&rev_head, curr->n);
 
-	rev_curr = *head;
+	rev_curr = rev_head;
 	for (curr = *head; curr != NULL; curr = curr->next)
 	{
 		if (curr->n != rev_curr->n)
