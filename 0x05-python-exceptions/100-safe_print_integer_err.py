9f0c650969bebbb0ab88f85028683631bbb602fc
renatoleonholberton <2829@holbertonschool.com>
2021-05-17 17:28:42 +0000

    Update except to handle all exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as err:
+    except Exception as err:
         stderr.write("Exception: {}\n".format(err))
 
     return False
