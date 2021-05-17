b4ed784e9a0ed89fec0ac2f254b45eee57c72efe
renatoleonholberton <2829@holbertonschool.com>
2021-05-17 15:26:27 +0000

    Rremove test code lines

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,18 +5,3 @@ def safe_print_integer(value):
     except Exception:
         return False
     return True
-
-value = 89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-    
-value = -89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "Holberton"
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
