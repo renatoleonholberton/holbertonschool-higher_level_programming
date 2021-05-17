21de15d058415f7129eb24df2bc3f14ccf6f49ae
renatoleonholberton <2829@holbertonschool.com>
2021-05-17 17:19:17 +0000

    Remove test code and use formt to print message

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,16 +6,5 @@ def safe_print_division(a, b):
     except Exception:
         result = None
     finally:
-        print("Inside result:", result)
+        print("Inside result: {}".format(result))
         return result
-
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
