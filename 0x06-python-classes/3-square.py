88454b89c849e06d17f231ee704b613541b5b794
renatoleonholberton <2829@holbertonschool.com>
2021-05-18 22:41:35 +0000

    Add docstrings

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""Simple Square class"""
+
+
 class Square:
+    """Simple class with attrs validation.
+
+    Args:
+        size (int): Size of the square
+
+    Raises:
+        TypeError: If size type is not int
+        ValueError: If size < 0
+    """
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError('size must be an integer')
@@ -9,4 +21,6 @@ class Square:
         self.__size = size
 
     def area(self):
+        """Method to calculate the area of the square.
+        """
         return self.__size ** 2
