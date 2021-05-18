e381ff9107b92d2ff656aff592bbe87c5ed523c7
renatoleonholberton <2829@holbertonschool.com>
2021-05-18 22:56:50 +0000

    Update docstring

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,7 +3,15 @@
 
 
 class Square:
-    """Simple class with attrs validation"""
+    """Simple class with attrs validation.
+
+    Args:
+        size (int): Size of the square
+
+    Raises:
+        TypeError: If size type is not int
+        ValueError: If size < 0
+    """
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError('size must be an integer')
