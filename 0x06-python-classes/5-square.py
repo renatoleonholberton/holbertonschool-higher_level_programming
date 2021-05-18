a356d67f32456b59293c4560f38ca3ec008825f3
renatoleonholberton <2829@holbertonschool.com>
2021-05-18 22:49:38 +0000

    Add docstrings

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
+"""Simple Square class"""
+
+
 class Square:
+    """Simple class with attrs validation.
+
+    Args:
+        size (int): Size of the square
+    """
     def __init__(self, size=0):
         self.__size = size
 
     def area(self):
+        """Method to calculate the area of the square"""
         return self.__size ** 2
 
     @property
     def size(self):
+        """size: property to modify private __size attr"""
         return self.__size
 
     @size.setter
@@ -20,10 +30,11 @@ class Square:
         self.__size = size
 
     def my_print(self):
+        """This function prints the square using '#' chars"""
         if self.__size == 0:
             print()
             return
-        
+
         rang = range(self.__size)
         for _ in rang:
             print('#' * self.__size)
