76fb863ce5a0329639375911092e859af6680121
renatoleonholberton <2829@holbertonschool.com>
2021-05-22 07:16:22 +0000

    Create a function that prints 'My name is <first name> <last name>'

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains say_my_name function"""
+
+
+def say_my_name(first_name, last_name=""):
+    """say_my_name: Prints My name is <first name> <last name>
+
+    Args:
+        first_name (str): Name as string
+        last_name (int): Last name as string
+    """
+
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+
+    print('My name is {} {}\n'.format(first_name, last_name))
