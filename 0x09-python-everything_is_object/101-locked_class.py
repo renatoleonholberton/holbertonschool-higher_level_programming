f04fe204208579d1b7cf9e475325d8d3b1acae59
renatoleonholberton <2829@holbertonschool.com>
2021-05-25 21:04:00 +0000

    Create a class that prevents the user from dynamically creating new instance attributes

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""LockedClass to prevent atribute creation"""
+
+
+class LockedClass:
+    """Class that prevent attribute ceation"""
+    def __setattr__(self, name, value):
+        if name != 'first_name':
+            raise AttributeError(
+                "'LockedClass' object has no attribute '{}'".format(name))
+        self.__dict__[name] = value
