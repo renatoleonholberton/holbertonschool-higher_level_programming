50baff46f85c5421b4a9496d2bfa42bff92c376e
renatoleonholberton <2829@holbertonschool.com>
2021-05-25 00:05:00 +0000

    Use property instead of private var to assign the value

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
             width (int): Rectangle's width
             heihgt (int): Rectangle's height
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
