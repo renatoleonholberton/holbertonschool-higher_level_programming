0abc33f1da06f02ca0a1baac23639acdb253307a
renatoleonholberton <2829@holbertonschool.com>
2021-05-25 23:57:00 +0000

    Use __slots__ private variable to avoid dynamic attribute creation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,8 +5,3 @@
 class LockedClass:
     """Class that prevent attribute ceation"""
     __slots__ = ['first_name']
-    def __setattr__(self, name, value):
-        if not name == 'first_name':
-            raise AttributeError(
-                "'LockedClass' object has no attribute '{}'".format(name))
-        self.__dict__[name] = value
