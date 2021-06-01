5685c7bd9cdfffb0ea145053ad7fabe64ce21b98
renatoleonholberton <2829@holbertonschool.com>
2021-06-01 23:52:21 +0000

    Create a class Square that implements __str__() method

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module with class definition"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """"Square class that inherits from Rectangle"""
+    def __init__(self, size):
+        super().__init__(size, size)
+        super().integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the rectangle"""
+        return self.__size ** 2
+
+    def __str__(self):
+        return '[Square] {}/{}'.format(self.__size, self.__size)
