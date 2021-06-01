5d63387b82d28262c7b1d1206190b2db833226c5
renatoleonholberton <2829@holbertonschool.com>
2021-06-01 21:46:02 +0000

    Create class with a method that raises an Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module with class definition"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def area(self):
+        """Raises an Exception"""
+        raise Exception('area() is not implemented')
