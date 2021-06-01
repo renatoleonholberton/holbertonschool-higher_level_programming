dc391f4b360c9dfa9da27cdb8b80ae196743dd79
renatoleonholberton <2829@holbertonschool.com>
2021-06-01 23:23:28 +0000

    Create a class with a method that validates a value

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module with class definition"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def area(self):
+        """Raises an Exception"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError('{:s} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{:s} must be greater than 0'.format(name))
