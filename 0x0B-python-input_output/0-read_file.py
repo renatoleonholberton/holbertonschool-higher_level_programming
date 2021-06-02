60d1f913091d7324e2ca8c50d2250a8b45d18346
renatoleonholberton <2829@holbertonschool.com>
2021-06-02 22:28:31 +0000

    Fix printing new line at the end

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Opens a file and prints its contents to stdout"""
     with open(filename, mode='r', encoding='utf-8') as file:
-        print(file.read())
+        print(file.read(), end="")
