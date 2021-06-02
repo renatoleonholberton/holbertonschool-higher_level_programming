7556d988880f27200b9819af9b41bb25e18d6926
renatoleonholberton <2829@holbertonschool.com>
2021-06-02 01:08:57 +0000

    Change order of statements to validate size first

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,8 +7,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """"Square class that inherits from Rectangle"""
     def __init__(self, size):
-        super().__init__(size, size)
         super().integer_validator('size', size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
