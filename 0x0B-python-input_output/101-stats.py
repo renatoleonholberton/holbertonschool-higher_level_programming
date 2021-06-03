bac282d8474c5518382a380fe18877bdb1e1bbf9
renatoleonholberton <2829@holbertonschool.com>
2021-06-03 04:50:36 +0000

    Back to previous version

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -21,7 +21,6 @@ try:
     for line in stdin:
         count += 1
         line_ls = line.split(' ')
-        
         status_code = line_ls[7]
         size += int(line_ls[-1])
 
@@ -32,7 +31,5 @@ try:
 
         if count % 10 == 0:
             print_stats(size, status_codes_metrics)
+finally:
     print_stats(size, status_codes_metrics)
-except KeyboardInterrupt:
-    print_stats(size, status_codes_metrics)
-    raise
