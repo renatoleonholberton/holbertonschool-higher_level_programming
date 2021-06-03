59ac391e67f08110f018cabe1a327e3ec6c4828e
renatoleonholberton <2829@holbertonschool.com>
2021-06-03 01:14:40 +0000

    Modify code to make use of json.dumps function

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,5 @@
 def save_to_json_file(my_obj, filename):
     """Parses string to object"""
     import json
-    obj_str = json.dumps(my_obj, sort_keys=True)
     with open(filename, mode='w', encoding='utf-8') as file:
-        file.write(obj_str)
+        json.dump(my_obj, file)
