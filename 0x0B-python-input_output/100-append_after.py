2240194549566ae3e14e48ff3db09313bdcc9f77
renatoleonholberton <2829@holbertonschool.com>
2021-06-03 02:27:34 +0000

    Add code to handle read and write separetely

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,9 +4,12 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """Adds a new line of test after each coincidence"""
-    with open(filename, mode='r+', encoding='utf-8') as file:
+    lines = []
+    with open(filename, mode='r', encoding='utf-8') as file:
         lines = file.readlines()
         for ind, line in enumerate(lines):
             if search_string in line:
                 lines[ind] += "{}".format(new_string)
+
+    with open(filename, mode='w', encoding='utf-8') as file:
         file.write("".join(lines))
