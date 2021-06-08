cc4bba3f524d8e2da05b8ddbbb1659f900da112a
renatoleon <2829@holbertonschool.com>
2021-06-08 19:17:10 +0000

    Add new condition to return an empty list as a string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -97,7 +97,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON representation of a list dictionary"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
 
         return json.dumps(list_dictionaries)
