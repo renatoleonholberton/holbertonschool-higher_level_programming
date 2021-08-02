9c20b4dfbd5140666c96601d1f1d0be98460a2a9
rennleon <2829@holbertonschool.com>
2021-08-02 17:27:05 +0000

    Update code to handle cases when the max is the first element

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,9 +6,11 @@ let secMax = 0;
 
 if (argv.length > 3) {
   for (let i = 3; i < argv.length; i++) {
-    if (Number(argv[i]) >= max) {
-      secMax = max;
-      max = Number(argv[i]);
+    if (Number(argv[i - 1]) > max) {
+      max = Number(argv[i - 1]);
+    }
+    if (Number(argv[i]) > secMax && Number(argv[i]) <= max) {
+      secMax = Number(argv[i]);
     }
   }
 }
