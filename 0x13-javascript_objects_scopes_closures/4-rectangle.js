e4bca85664c3c149b28b796d2089ae8b89b4454f
rennleon <2829@holbertonschool.com>
2021-08-03 17:30:27 +0000

    Fix 'height' typo

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
@@ -9,24 +9,19 @@ module.exports = class Rectangle {
   }
 
   print () {
-    let row = '';
-
-    for (let i = 0; i < this.width; i++) {
-      row += 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(row);
-    }
+    for (let i = 0; i < this.height; i++) { console.log('X'.repeat(this.width)); }
   }
 
   rotate () {
     const tmp = this.width;
     this.width = this.height;
-    this.heigth = tmp;
+    this.height = tmp;
   }
 
   double () {
     this.width *= 2;
     this.height *= 2;
   }
-};
+}
+
+module.exports = Rectangle;
