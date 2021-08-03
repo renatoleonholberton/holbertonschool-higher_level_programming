97357baed02f14582807b4316c7e3bee4ab130df
rennleon <2829@holbertonschool.com>
2021-08-03 15:12:41 +0000

    Refactor code

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,13 +3,8 @@
 const dict = require('./101-data.js').dict;
 
 const obj = Object.entries(dict)
-  .reduce((acc, [key, value]) => {
-    if (!acc[value]) {
-      acc[value] = [key];
-    } else {
-      acc[value].push(key);
-    }
-    return acc;
-  }, {});
+  .reduce((acc, [key, value]) => acc[value]
+    ? { ...acc, [value]: [...acc[value], key] }
+    : { ...acc, [value]: [key] }, {});
 
 console.log(obj);
