920983d674cc5424fc38473be2106ba7635e035a
rennleon <2829@holbertonschool.com>
2021-08-03 17:50:04 +0000

    Remove 'size' attr creation

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,17 +4,8 @@ const Sq = require('./5-square.js');
 
 class Square extends Sq {
   charPrint (c = 'X') {
-    if (!this.size) {
-      return;
-    }
-
-    let side = '';
-
-    for (let i = 0; i < this.size; i++) {
-      side += c;
-    }
-    for (let i = 0; i < this.size; i++) {
-      console.log(side);
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
