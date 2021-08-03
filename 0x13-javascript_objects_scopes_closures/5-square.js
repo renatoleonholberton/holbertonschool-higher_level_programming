0929526da093fb38efeda4a99f76acb0b7f50971
rennleon <2829@holbertonschool.com>
2021-08-03 17:40:47 +0000

    Remove condition to only work with width and height

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -5,10 +5,6 @@ const Rectangle = require('./4-rectangle.js');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-
-    if (size > 0) {
-      this.size = size;
-    }
   }
 }
 
