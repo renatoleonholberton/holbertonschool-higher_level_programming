3764e1c70e8547dede8e6fe37f1b1c0ba1985c45
rennleon <2829@holbertonschool.com>
2021-08-07 10:33:56 +0000

    Add import statement

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.schema import ForeignKey
 
-Base = declarative_base()
+from relationship_state import Base
 
 
 class City (Base):
