3d71b63fcb9131175c416ed9e546646afc416299
rennleon <2829@holbertonschool.com>
2021-08-07 09:26:01 +0000

    Create a script that prints all City objects from the database hbtn_0e_14_usa

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script to print all city objects
+"""
+import sys
+from model_state import State
+from model_city import City
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    user, passwd, dbname = sys.argv[1:]
+
+    conn_str = 'mysql+mysqldb://{}:{}@localhost/{}'\
+        .format(user, passwd, dbname)
+
+    engine = create_engine(conn_str, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    res = session.query(State.name, City.id, City.name)\
+        .join(City, State.id == City.state_id)\
+        .order_by(City.id)\
+        .all()
+
+    for r in res:
+        print('{}: ({}) {}'.format(*r))
