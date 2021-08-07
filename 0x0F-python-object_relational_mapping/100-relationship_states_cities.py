8c9b6455c2bc2110622c562799c6fc831eb5cc32
rennleon <2829@holbertonschool.com>
2021-08-07 10:30:12 +0000

    Fix module import

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -2,8 +2,8 @@
 """Script to print all city objects
 """
 import sys
-from model_state import Base, State
-from model_city import City
+from relationship_state import Base, State
+from relationship_city import City
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
