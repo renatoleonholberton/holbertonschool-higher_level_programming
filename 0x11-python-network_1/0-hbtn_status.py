786286460733792b570bb0bd839ed2a72a9f0d17
rennleon <2829@holbertonschool.com>
2021-08-20 19:08:13 +0000

    Update output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,7 @@ from urllib import request as rq
 if __name__ == '__main__':
     with rq.urlopen('https://intranet.hbtn.io/status') as res:
         _bytes = res.read()
+        print('Body response:')
         print('\t- type: {}'.format(type(_bytes)))
         print('\t- content: {}'.format(_bytes))
         print('\t- utf8 content: {}'.format(_bytes.decode('utf-8')))
