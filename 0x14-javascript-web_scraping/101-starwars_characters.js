d878ddfeea95809ddfc42969b60f88e1a77d1fb4
rennleon <2829@holbertonschool.com>
2021-09-14 19:22:27 +0000

    Modify code to make sequential requests

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,16 +6,23 @@ const rq = require('request');
 const id = argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${id}`;
 
+function printActors (actorsUrls, i) {
+  if (i >= actorsUrls.length) { return; }
+
+  const curl = actorsUrls[i];
+  rq(curl, (err, data, body) => {
+    if (err) { return; }
+
+    const actor = JSON.parse(body);
+    console.log(actor.name);
+
+    printActors(actorsUrls, i + 1);
+  });
+}
+
 rq(url, function (err, res, body) {
   if (err) { return; }
 
   const film = JSON.parse(body);
-  for (const curl of film.characters) {
-    rq(curl, (cerr, cdata, cbody) => {
-      if (err) { return; }
-
-      const actor = JSON.parse(cbody);
-      console.log(actor.name);
-    });
-  }
+  printActors(film.characters, 0);
 });
